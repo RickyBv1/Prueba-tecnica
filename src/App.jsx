9bfc37ccb27a48ea3f00a5701258dc129b649be0
RickyBv1 <rickycortes668668@gmail.com>
2025-04-03 17:09:10 -0400

    fetch random cat facts and display in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=&color=red&json=true`
 
 function App() {
 
+  const [fact, setFact] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
   return (
-    <>
-    </>
+    <main>
+      <h1>Cats app</h1>
+      {fact && <p>{fact}</p>}
+    </main>
   )
 }
 
