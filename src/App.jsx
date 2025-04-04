708082c72aafb51792f144a603837b24108a78d2
RickyBv1 <rickycortes668668@gmail.com>
2025-04-03 20:51:46 -0400

    refactor: extract random fact fetching logic into a separate service and add button to fetch new facts

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,54 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import getRandomFact from "./services/facts";
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?size=&color=red&json=true`
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 function App() {
-
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   //Para recuperar la cita al cargar la pagina
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }, [])
+    getRandomFact().then(setFact);
+  }, []);
 
   //Para recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
-    if (!fact) return
+    if (!fact) return;
 
-    const firstThreeWords = fact.split(' ', 3).join(' ')
+    const firstThreeWords = fact.split(" ", 3).join(" ");
 
-        fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
-  }, [fact])
+    fetch(
+      `https://cataas.com/cat/says/${firstThreeWords}?size=&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact();
+    setFact(newFact);
+  };
 
   return (
     <main>
       <h1>Cats app</h1>
+      <button onClick={handleClick}>Get a new fact!</button>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words of the fact: ${fact}`} />}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={`Image extracted using the first three words of the fact: ${fact}`}
+        />
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
